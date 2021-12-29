c7c7c9f99953256198b917697307e4a47b2b51b4
Kanderua <1073348168@qq.com>
2021-12-29 16:58:09 +0800

    ignore

--- realtime/src/main/java/com/atguigu/realtime/util/JdbcUtil.java
+++ realtime/src/main/java/com/atguigu/realtime/util/JdbcUtil.java
@@ -22,7 +22,7 @@ public class JdbcUtil {
                                         String sql,
                                         Object[] args ,
                                         Class<T> tClass){
-        System.out.println("test11");
+        System.out.println("test121");
         return null;
     }
 }
